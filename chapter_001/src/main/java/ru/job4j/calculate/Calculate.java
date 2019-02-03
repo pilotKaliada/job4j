882048b8c3de0a13ac8e7a96593b10749b914c5d
pilotKaliada <kaliada-biz@yandex.ru>
2019-02-03 17:05:24 +0300

    1.3. JavaDoc. Code convention[#109960]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j.calculate;
+/**
+* Calculate
+* @author Alexander Koleda (kaliada-biz@yandex.ru)
+*/
 public class Calculate {
+	/**
+	* Main, вывод строки на консоль.
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 			System.out.println("Hello World");
 	}
