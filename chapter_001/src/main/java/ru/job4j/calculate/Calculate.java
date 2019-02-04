217c52ab05aa856ee3f8eaf9f6d53bb26e86f684
pilotKaliada <kaliada-biz@yandex.ru>
2019-02-04 21:38:16 +0300

    1.4. JUnit[#109961]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -5,11 +5,19 @@ package ru.job4j.calculate;
 */
 public class Calculate {
 	/**
-	* Main, вывод строки на консоль.
+	* Method main, вывод строки на консоль.
 	* @param args - args.
 	*/
 	public static void main(String[] args) {
 			System.out.println("Hello World");
+	}
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+			return "Echo, echo, echo : " + name;
 	}
 }
 	
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexander Koleda (kaliada-biz@yandex.ru)
+* @version 1.0
+* @since 1.0 03/02/2019
+*/
+package ru.job4j.calculate;
