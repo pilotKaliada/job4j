41221c3fc189c1a206a6cd3c6e3af6b6d4f5604e
pilotKaliada <kaliada-biz@yandex.ru>
2019-02-17 22:38:02 +0300

    4.2. Максимум из двух чисел[#109924]Тернарным условием

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -9,15 +9,15 @@ public class Max {
     /**
      * Сравнивает числа и возрващает болшее.
      *
-     * @param first первое число.
+     * @param first  первое число.
      * @param second второе число
      * @return большее число.
      */
     public int max(int first, int second) {
-        if (first > second) {
-            return first;
-        } else {
-            return second;
-        }
+
+        return (first > second) ? first : second;
+
+
     }
 }
+
