e8e2be57603eef384d43d60b59c1323001ad3f1c
pilotKaliada <kaliada-biz@yandex.ru>
2019-02-17 14:18:03 +0300

    4.1. Глупый бот.[#109922]

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexander Koleda (kaliada-biz@yandex.ru)
+ * @version 1.0
+ * @since 1.0
+ */
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByeBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(bot.answer("Кто будет вторым президетном Беларуси?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}
