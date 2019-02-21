876a9ffd949deb981a7b0af80d3ee6ef27f4a731
pilotKaliada <kaliada-biz@yandex.ru>
2019-02-22 00:15:01 +0300

    5.5. Рефакторинг кода.[#109929]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Alexander Koleda (kaliada-biz@yandex.ru)
  * @version 1.0
@@ -14,18 +16,10 @@ public class Paint {
      * @return строка в виде пирамиды
      */
     public String piramid(int height) {
-        StringBuilder screen = new StringBuilder();
-        for (int j = 0; j < height; j++) {
-            for (int i = 1; i < height * 2; i++) {
-                if (i >= height - j && i <= height + j) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height * 2 - 1,
+                (j, i) -> i >= height - j - 1 && i <= height + j - 1);
     }
 
     /**
@@ -35,28 +29,35 @@ public class Paint {
      * @return строка в виде пирамиды
      */
     public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        for (int j = 0; j < height; j++) {
-            for (int i = 0; i < height; i++) {
-                if (i <= j) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (j, i) -> i <= j);
     }
 
+    /**
+     * Строит левую полупирамиду заданной высоты
+     *
+     * @param height высота пирамиды.
+     * @return строка в виде пирамиды
+     */
+
     public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (j, i) -> i >= height - j - 1);
+    }
+
+
+    private String loopBy(int height, int widht, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
         for (int j = 0; j < height; j++) {
-            for (int i = 0; i < height; i++) {
-                if (i < height - j - 1) {
-                    screen.append(" ");
-                } else {
+            for (int i = 0; i < widht; i++) {
+                if (predict.test(j, i)) {
                     screen.append("^");
+                } else {
+                    screen.append(" ");
                 }
             }
             screen.append(System.lineSeparator());
