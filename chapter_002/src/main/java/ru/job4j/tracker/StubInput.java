6be10c8e21fce690b31ac811a7a304d4ed217183
pilotKaliada <kaliada-biz@yandex.ru>
2019-06-10 23:46:21 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#110004]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -1,19 +1,31 @@
 package ru.job4j.tracker;
 
 public class StubInput implements Input {
-    private String [] answers;
+    private String[] answers;
     private int position;
 
-    public StubInput(String [] answers ){
-        this.answers=answers;
+    public StubInput(String[] answers) {
+        this.answers = answers;
     }
-    public String ask(String question){
+
+    public String ask(String question) {
         return this.answers[this.position++];
     }
 
     @Override
     public int ask(String question, int[] range) {
-        return Integer.valueOf(this.ask(question));
-        //throw new UnsupportedOperationException("Unsupported operation");
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int value : range) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu range");
+        }
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,18 +1,27 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Input input;
+
+    public ValidateInput(Input input){
+        this.input = input;
+    }
+    public String ask (String question){
+        return this.input.ask(question);
+    }
+
     public  int ask (String question, int[] range){
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe){
                 System.out.println("Please select key from menu.");
             }
             catch (NumberFormatException nfe){
-                System.out.println("Please enter validate date again.");
+                System.out.println("Please enter validate data again.");
             }
 
         } while (invalid);
